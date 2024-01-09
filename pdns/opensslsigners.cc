4c1d457410bf7808fc6a382684b12d90fb358602
jgoertzen-sb <auto@merge.org>
2024-01-09 13:05:45 -0600

    fixed some more unique_ptr issues

--- pdns/opensslsigners.cc
+++ pdns/opensslsigners.cc
@@ -1961,16 +1961,16 @@ void OpenSSLPQCDNSCryptoKeyEngine::fromISCMap(DNSKEYRecordContent& drc, std::map
 	if (!ctx) {
           throw std::runtime_error(getName() + " could not initalize PKEY_CTX");
 	}
-	auto pk = std::unique_ptr<EVP_PKEY, void (*)(EVP_PKEY*)>(nullptr, EVP_PKEY_free);
+	EVP_PKEY *pk = nullptr;
 	if (EVP_PKEY_fromdata_init(ctx.get()) <= 0 ||
-	    EVP_PKEY_fromdata(ctx.get(), &(pk.get()), EVP_PKEY_KEY_PARAMETERS, params) <= 0)
+	    EVP_PKEY_fromdata(ctx.get(), &pk, EVP_PKEY_KEY_PARAMETERS, params) <= 0)
 	{
           throw std::runtime_error(getName() + " fromdata failed");
 	}
 	if (!pk) {
           throw std::runtime_error(getName() + " fromdata succeeded but pk is NULL");
 	}
-	d_pqckey = pk;
+	d_pqckey = std::unique_ptr<EVP_PKEY, void (*)(EVP_PKEY*)>(pk, EVP_PKEY_free);
 }
 
 void OpenSSLPQCDNSCryptoKeyEngine::fromPublicKeyString(const std::string& content)
